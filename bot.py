ffefa23e8b95547368f452e05b6bc254c03c8c90
danya4442 <145836520+danya4442@users.noreply.github.com>
2025-04-20 20:28:37 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,18 +5,21 @@ from random import choice
 from logic_ai import Text2ImageAPI
 import base64
 import random
+import pyjokes
 
 bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("📚 Учёба")
-    btn2 = types.KeyboardButton("🎮 Играть")
-    btn3 = types.KeyboardButton("❓ Задать вопрос")
-    btn4 = types.KeyboardButton("🌅 Генератор картинок")
-    markup.add(btn1, btn2, btn3, btn4)
+    btn1 = types.KeyboardButton("❓ Задать вопрос")
+    btn2 = types.KeyboardButton("📚 Учёба")
+    btn3 = types.KeyboardButton("🎮 Играть")
+    btn4 = types.KeyboardButton("😂 Генератор шуток")
+    btn5 = types.KeyboardButton("⭐️ Оценить")
+    markup.add(btn1, btn2, btn3, btn4, btn5)
     bot.send_message(message.chat.id, text="Привет, {0.first_name}! Я телеграмм-бот онлайн-школы, который обеспечит тебя и других учеников удобными и эффективными инструментами для обучения.".format(message.from_user), reply_markup=markup)
+    
               
 @bot.message_handler(content_types=['text'])
 def func(message):
@@ -40,25 +43,28 @@ def func(message):
 
     elif(message.text == "❓ Задать вопрос"):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Как меня зовут?")
+        # btn1 = types.KeyboardButton("Как меня зовут?")
         btn2 = types.KeyboardButton("Что я могу?")
         back = types.KeyboardButton("🔙 Вернуться в главное меню")
-        markup.add(btn1, btn2, back)
+        markup.add (btn2, back) #btn1
         bot.send_message(message.chat.id, text="Задай мне вопрос", reply_markup=markup)
     
-    elif(message.text == "Как меня зовут?"):
-        bot.send_message(message.chat.id, "У меня нет имени..")
-    
     elif message.text == "Что я могу?":
-        bot.send_message(message.chat.id, text="Поздороваться с читателями")
+        bot.send_message(message.chat.id, text="""\
+    1)Прислать расписание уров или расписание звонков 
+2)Поиграть в игру Орёл или Решка 
+3)Сгенерировать шутку на английском 
+4)Оценить бота\
+""")
     
     elif (message.text == "🔙 Вернуться в главное меню"):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton("📚 Учёба")
-        button2 = types.KeyboardButton("🎮 Играть")
-        button3 = types.KeyboardButton("❓ Задать вопрос")
-        button4 = types.KeyboardButton("🌅 Генератор картинок")
-        markup.add(button1, button2, button3, button4)
+        button1 = types.KeyboardButton("❓ Задать вопрос")
+        button2 = types.KeyboardButton("📚 Учёба")
+        button3 = types.KeyboardButton("🎮 Играть")
+        button4 = types.KeyboardButton("😂 Генератор шуток")
+        button5 = types.KeyboardButton("⭐️ Оценить")
+        markup.add(button1, button2, button3, button4, button5)
         bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
 
     elif (message.text == "🎮 Играть"):
@@ -72,31 +78,41 @@ def func(message):
         coin = choice(["Орёл", "Решка"])
         bot.reply_to(message, coin)
 
-    elif(message.text == "🌅 Генератор картинок"):
+    elif (message.text == "⭐️ Оценить"):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        buttn1 = types.KeyboardButton("Сгенерировать картинку")
+        buton1 = types.KeyboardButton("5 звезд ⭐️")
+        buton2 = types.KeyboardButton("4 звезды ⭐️")
+        buton3 = types.KeyboardButton("3 звезды ⭐️")
+        buton4 = types.KeyboardButton("2 звезды ⭐️")
+        buton5 = types.KeyboardButton("1 звезда ⭐️")
+        back = types.KeyboardButton("🔙 Вернуться в главное меню")
+        markup.add(buton1, buton2, buton3, buton4, buton5, back)
+        bot.send_message(message.chat.id, text="Оцени бота по 5-ти бальной шкале", reply_markup=markup)
+
+    elif (message.text == "5 звезд ⭐️"):
+        bot.send_message(message.chat.id, text="Спасибо за оценку!")
+
+    elif (message.text == "4 звезды ⭐️"):
+        bot.send_message(message.chat.id, text="Спасибо за оценку!")
+
+    elif (message.text == "3 звезды ⭐️"):
+        bot.send_message(message.chat.id, text="Спасибо за оценку!")
+
+    elif (message.text == "2 звезды ⭐️"):
+        bot.send_message(message.chat.id, text="Что нам следует исправить?")
+
+    elif (message.text == "1 звезда ⭐️"):
+        bot.send_message(message.chat.id, text="Что нам следует исправить?")
+
+    elif(message.text == "😂 Генератор шуток"):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        buttn1 = types.KeyboardButton("Сгенерировать шутку")
         buttn2 = types.KeyboardButton("🔙 Вернуться в главное меню")
         markup.add(buttn1, buttn2)
         bot.send_message(message.chat.id, text="Нажми на кнопку", reply_markup=markup)
 
-    elif (message.text == "Сгенерировать картинку"):
-            promt = message.text
-
-            send_message = bot.reply_to(message, "Генерирую картинку...")
-            bot.send_chat_action(message.chat.id, "typing")
-
-            api = Text2ImageAPI('https://api-key.fusionbrain.ai/', api_token, secret_key)
-            model_id = api.get_model()
-            uuid = api.generate(promt, model_id)
-            images = api.check_generation(uuid)
-            image_base64 = images[0]
-            image_data = base64.b64decode(image_base64)
-            file_name = "generated_image"+str(random.randint(0, 1000))+".jpg"
-            with open(f"img/{file_name}", "wb") as file:
-                file.write(image_data)
-            with open(f"img/{file_name}", "rb") as file:
-                bot.send_photo(message.chat.id, file)
-
-            bot.delete_message(message.chat.id, send_message.message_id)
+    elif (message.text == "Сгенерировать шутку"):
+        joke = pyjokes.get_joke(language="en", category="all")
+        bot.send_message(message.chat.id, joke)
 
 bot.polling(none_stop=True)
